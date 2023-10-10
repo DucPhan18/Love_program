644255331e88f768f5dab7c967575e290088cc9f
DucPhan18 <99118919+DucPhan18@users.noreply.github.com>
2023-10-10 17:07:11 +0700

    C++

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "!!!Luu y: Nhap ten khong dau!!!" << endl;
+    cout << "Nhap ten ban:"<<endl;
+    string n;
+    getline (cin, n);
+    cout << " ++ ++ " <<endl;
+    cout << "+  +  +" <<endl;
+    cout << " +   +" <<endl;
+    cout <<"   +  " << endl;
+    cout << "To thich cau ";
+    cout << n ;
+
+
+    return 0;
+}
